26529bfa4e2c536480540ad24d3e0f3a9beebdf3
Allison Summers <allisonsummers44@gmail.com>
2018-01-18 11:57:29 -0500

    Added People and Starships

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
 
 $(() => {
-    let loaded = false
-    $('#infoButton').click(() => {
-      if(!loaded) { 
-        $.ajax({   ///Ajax is a jQuery method /// API is inside and object notation
+
+  let loaded = false
+
+  $('#peopleButton').click(() => {
+    if(!loaded) {
+     loaded = true
+    $.ajax({
         type: 'GET',
-        url: 'https://swapi.co/api/people'
+        url: 'https://swapi.co/api/people' //'results' attribute is gained from here
       }).done((res) => {
         let people = res.results
         for(p of people) {
-         $('#tableBody').append(createTableRow(p));    //   tableBody.append(person) Adds person to html list--but isn't formated in the table rows
+          $('#tableBody').append(createTableRow(p))
         }
+        
       })
     }
-      
-    })
+  })
 
-    function createTableRow(person) {
-        let row = $(`<tr></tr>`); ///Jquery element creates an object that represents HTML element
-        let name = $(`<td>${person.name}</td>`);
-        let height = $(`<td>${person.height}</td>`);
-        let birth = $(`<td>${person.birth_year}</td>`); /// use console log to look up properties of {person}
-        let hair = $(`<td>${person.hair_color}</td>`)
-
-        row.append(name)
-        row.append(height)
-        row.append(birth)
-        row.append(hair)
-
-        return row
-
+  $('#starshipButton').click(() => {
+    if(!loaded) {
+      loaded = true
+      $.ajax({
+        type: 'GET',
+        url: 'https://swapi.co/api/starships'
+      }).done((res) => {
+        let ships = res.results
+        for(star of ships) {
+          $('#tableBody').append(createTableRowStar(star))
+        }
+        
+      })
 
     }
-  
   })
+
+  function createTableRow(person) {
+    let row = $(`<tr></tr>`)
+    let name = $(`<td>${person.name}</td>`)
+    let height = $(`<td>${person.height}</td>`)
+    let birth = $(`<td>${person.birth_year}</td>`)
+
+    row.append(name, height, birth)
+    return row;
+  
+  }
+
+  function createTableRowStar(starship) {
+    let row = $(`<tr></tr>`)
+    let model = $(`<td>${starship.model}</td>`)
+    let manufacturer = $(`<td>${starship.manufacturer}</td>`)
+    let starName = $(`<td>${starship.name}</td>`)
+  
+    
+    row.append(model, manufacturer, starName)
+
+    return row
+
+  }
+
+
+
+  $('#clearButton').click(() => {
+    $('#tableBody').empty();
+    loaded = false;
+  })
+
+})
